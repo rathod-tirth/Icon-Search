75cf22b6e005a7068e3f74a62a7a74f28ef6bf80
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-14 12:12:31 +0530

    update grid view and button variant

--- iconsearch/src/Page/Home.jsx
+++ iconsearch/src/Page/Home.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { Alert, Button, Grid } from '@mui/material'
 import SimpleCard from '../Component/SimpleCard'
 import * as fab from '@fortawesome/free-brands-svg-icons';
 import Header2 from '../Component/Header2';
 
+const originaList = Object.values(fab).slice(0, -2);
+
 function Home() {
-    const originaList = Object.values(fab).slice(0, 200);
     // font Awesome icon list
     const [iconArray, setIconArray] = useState(originaList)
     // for alert box of copy
     const [isCopy, setIsCopy] = useState(false);
 
-    // to filter the iconArray
+    // // to filter the iconArray
     const handleFilter = (value) => {
         if (value !== "") {
             const newIconArray = originaList.filter(icon => icon.iconName.toLowerCase().includes(value.toLowerCase()));
@@ -34,11 +35,11 @@ function Home() {
             {/* alert */}
             {isCopy && <Alert severity="success">Font Copied.</Alert>}
             {/* font card */}
-            <Grid container spacing={2}>
+            <Grid container spacing={1}>
                 {iconArray.map((icon, index) => {
                     return (
-                        <Grid item sm={2} key={index} >
-                            <Button size='small' variant='outlined' style={{ width: "10rem", color: "black", borderColor: "black" }} onClick={() => copyToClipboard(icon.iconName)}>
+                        <Grid item xs={1} key={index} >
+                            <Button size='small' variant='text' color='info' style={{ width: "5rem", height: "5rem", }} onClick={() => copyToClipboard(icon.iconName)}>
                                 <SimpleCard icon={icon} />
                             </Button>
                         </Grid>
