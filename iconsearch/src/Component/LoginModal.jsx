3ee049c4a2c7d7446e155e9041bfc292d3097169
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-14 11:10:33 +0530

    remove logs and update indentation with fix typos

--- iconsearch/src/Component/LoginModal.jsx
+++ iconsearch/src/Component/LoginModal.jsx
@@ -7,51 +7,51 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import axios from 'axios'
 
-export default function LoginModal({isLoginModal, setIsLoginModal}) {
-    // form control
-    const [formValue,setFormValue]=useState({
-        username:"",
-        password:""
-    })
+export default function LoginModal({ isLoginModal, setIsLoginModal }) {
+  // form control
+  const [formValue, setFormValue] = useState({
+    username: "",
+    password: ""
+  })
 
-    // saves the form value to the state
-    const handleChange=(e)=>{
-        setFormValue({...formValue, [e.target.name]:e.target.value})
+  // saves the form value to the state
+  const handleChange = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value })
+  }
+
+  // check the value and return the result
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.get("http://localhost:3000/user")
+      const userData = resp.data
+
+      const user = userData.find(user => user.username === formValue.username && user.password === formValue.password);
+      console.log(user);
+      if (user) {
+        // Successful login, proceed with further actions
+        console.log('Login successfull');
+      } else {
+        // Invalid credentials, display error message
+        console.log("Invalid Email and Password");
+      }
+
+    } catch (e) {
+      console.log(e);
     }
-
-    // check the value and return the result
-    const handleSubmit=async(e)=>{
-        e.preventDefault();
-        try {
-            const resp = await axios.get("http://localhost:3000/user")
-            const userData=resp.data
-
-            const user = userData.find(user => user.username === formValue.username && user.password === formValue.password);
-            console.log(user);
-            if (user) {
-                // Successful login, proceed with further actions
-                console.log('Login successful');
-            } else {
-                // Invalid credentials, display error message
-                console.log("Invalid Email and Password");
-            }
-
-        } catch (e) {
-            console.log(e);
-        }
-        setIsLoginModal(false);
-    }
+    setIsLoginModal(false);
+  }
 
   return (
     <>
-      <Dialog open={isLoginModal} onClose={()=>setIsLoginModal(false)}>
+      <Dialog open={isLoginModal} onClose={() => setIsLoginModal(false)}>
         <DialogTitle>Login</DialogTitle>
         <DialogContent>
           <TextField autoFocus required margin="dense" id="username" name="username" label="Username" type="text" fullWidth variant="standard" onChange={handleChange} value={formValue.username} />
           <TextField required margin="dense" id="password" name="password" label="Password" type="password" fullWidth variant="standard" onChange={handleChange} value={formValue.password} />
         </DialogContent>
         <DialogActions>
-          <Button onClick={()=>setIsLoginModal(false)} >Cancel</Button>
+          <Button onClick={() => setIsLoginModal(false)} >Cancel</Button>
           <Button type="submit" onClick={handleSubmit}>Login</Button>
         </DialogActions>
       </Dialog>
--- iconsearch/src/Component/SignUpModal.jsx
+++ iconsearch/src/Component/SignUpModal.jsx
@@ -7,46 +7,45 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import axios from 'axios'
 
-export default function SignUpModal({isSignUpModal, setIsSignUpModal}) {
-    // form control
-    const [formValue,setFormValue]=useState({
-        id:"",
-        username:"",
-        password:""
-    })
+export default function SignUpModal({ isSignUpModal, setIsSignUpModal }) {
+  // form control
+  const [formValue, setFormValue] = useState({
+    id: "",
+    username: "",
+    password: ""
+  })
 
-    // saves the form value to the state
-    const handleChange=(e)=>{
-        setFormValue({...formValue, id:new Date().getTime().toString().slice(8, 12), [e.target.name]:e.target.value})
+  // saves the form value to the state
+  const handleChange = (e) => {
+    setFormValue({ ...formValue, id: new Date().getTime().toString().slice(8, 12), [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault()
+      const res = await axios.post("http://localhost:3000/user", formValue);
+      if (res.status === 201) {
+        localStorage.setItem("userId", formValue.id);
+        setFormValue({ id: "", username: "", password: "" })
         console.log(formValue);
+        console.log("Signed Up successfully");
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-    const handleSubmit = async (e) => {
-        try {
-          e.preventDefault()
-            const res = await axios.post("http://localhost:3000/user", formValue);
-            if (res.status === 201) {
-              localStorage.setItem("userId", formValue.id);
-              setFormValue({ id: "", username: "", password: ""})
-              console.log(formValue);
-              console.log("Signed Up successfully");
-            }
-        } catch (error) {
-          console.log(error);
-        }
-        setIsSignUpModal(false)
-      };
+    setIsSignUpModal(false)
+  };
 
   return (
     <>
-      <Dialog open={isSignUpModal} onClose={()=>setIsSignUpModal(false)}>
+      <Dialog open={isSignUpModal} onClose={() => setIsSignUpModal(false)}>
         <DialogTitle>SignUp</DialogTitle>
         <DialogContent>
           <TextField autoFocus required margin="dense" id="username" name="username" label="Username" type="text" fullWidth variant="standard" onChange={handleChange} value={formValue.username} />
           <TextField required margin="dense" id="password" name="password" label="Password" type="password" fullWidth variant="standard" onChange={handleChange} value={formValue.password} />
         </DialogContent>
         <DialogActions>
-          <Button onClick={()=>setIsSignUpModal(false)} >Cancel</Button>
+          <Button onClick={() => setIsSignUpModal(false)} >Cancel</Button>
           <Button type="submit" onClick={handleSubmit}>SignUp</Button>
         </DialogActions>
       </Dialog>
