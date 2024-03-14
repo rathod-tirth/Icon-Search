70a0c620c99274adecbcf40b0dbca40e08c83920
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-14 11:09:41 +0530

    update indentation

--- iconsearch/src/Component/SimpleCard.jsx
+++ iconsearch/src/Component/SimpleCard.jsx
@@ -1,20 +1,19 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function SimpleCard({icon}) {
+export default function SimpleCard({ icon }) {
   return (
-      <CardContent >
-      <Typography variant="h5" component="div" sx={{textAlign:"center"}}>
+    <CardContent >
+      <Typography variant="h5" component="div" sx={{ textAlign: "center" }}>
         {/* icon */}
         <FontAwesomeIcon icon={icon} size="2x" />
-        </Typography>
-        <Typography sx={{textAlign:"center"}} color="text.secondary" gutterBottom>
+      </Typography>
+      <Typography sx={{ textAlign: "center" }} color="text.secondary" gutterBottom>
         {/* iconName */}
-          {icon.iconName}
-        </Typography>
-      </CardContent>
+        {icon.iconName}
+      </Typography>
+    </CardContent>
   );
 }
