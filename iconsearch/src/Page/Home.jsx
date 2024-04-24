587f3c8a438e52349ae4f833c796e0d5f7901102
Tirth <tirth.rathod@naapbooks.com>
2024-04-24 16:32:06 +0530

    add loader and reduce lag with smooth searchbar

--- iconsearch/src/Page/Home.jsx
+++ iconsearch/src/Page/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useMemo, useState } from 'react'
-import { Alert, Button, Grid } from '@mui/material'
+import React, { useMemo, useState, useTransition } from 'react'
+import { Alert, Button, CircularProgress, Grid } from '@mui/material'
 import SimpleCard from '../Component/SimpleCard'
 import * as fab from '@fortawesome/free-brands-svg-icons';
 import Header2 from '../Component/Header2';
@@ -12,14 +12,18 @@ function Home() {
     // for alert box of copy
     const [isCopy, setIsCopy] = useState(false);
 
-    // // to filter the iconArray
+    const [isPending, startTransition] = useTransition()
+
+    // to filter the iconArray
     const handleFilter = (value) => {
-        if (value !== "") {
-            const newIconArray = originaList.filter(icon => icon.iconName.toLowerCase().includes(value.toLowerCase()));
-            setIconArray(newIconArray)
-        } else {
-            setIconArray(originaList)
-        }
+        startTransition(() => {
+            if (value !== "") {
+                const newIconArray = originaList.filter(icon => icon.iconName.toLowerCase().includes(value.toLowerCase()));
+                setIconArray(newIconArray)
+            } else {
+                setIconArray(originaList)
+            }
+        })
     }
 
     // copies to clipboard
@@ -35,17 +39,31 @@ function Home() {
             {/* alert */}
             {isCopy && <Alert severity="success">Font Copied.</Alert>}
             {/* font card */}
-            <Grid container spacing={1}>
-                {iconArray.map((icon, index) => {
-                    return (
-                        <Grid item xs={1} key={index} >
-                            <Button size='small' variant='text' color='info' style={{ width: "5rem", height: "5rem", }} onClick={() => copyToClipboard(icon.iconName)}>
-                                <SimpleCard icon={icon} />
-                            </Button>
-                        </Grid>
-                    )
-                })}
-            </Grid>
+            {isPending ? (
+                <Grid
+                    container
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    style={{ minHeight: '100vh' }}
+                >
+                    <Grid item>
+                        <CircularProgress />
+                    </Grid>
+                </Grid>
+            ) : (
+                <Grid container spacing={1}>
+                    {iconArray.map((icon, index) => {
+                        return (
+                            <Grid item xs={1} key={index} >
+                                <Button size='small' variant='text' color='info' style={{ width: "5rem", height: "5rem", }} onClick={() => copyToClipboard(icon.iconName)}>
+                                    <SimpleCard icon={icon} />
+                                </Button>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            )}
 
         </div>
     )
